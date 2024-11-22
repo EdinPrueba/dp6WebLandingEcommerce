a3ad16a74ed5063f1b7c7e83f290e22a2fb5770e
Christopher <jazielquine@gmail.com>
2024-11-22 11:28:10 -0500

    fix: Formateo product-view

--- src/components/products/product-view.vue
+++ src/components/products/product-view.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="product-view">
-	  	<ProductConversionsHeader
+	<div class="product-view">
+		<ProductConversionsHeader
 			:show-unit="showUnity"
 			:default-unit="data.unit"
 			:conversions="data.conversions"
@@ -8,81 +8,75 @@
 			@unit-selection="unitSelection"
 		/>
 		<div class="btns-product-view" v-if="webLocalImages.length > 0">
-			<button 
-				v-for="(image, index) in webLocalImages" 
+			<button
+				v-for="(image, index) in webLocalImages"
 				:key="image.id"
 				class="btn-product-view"
-				:class="{'select' : image.select, 'not-select' : !image.select}"
-				:style="{'border-color': image.select ? globalColors.secondary : null}"
+				:class="{ select: image.select, 'not-select': !image.select }"
+				:style="{
+					'border-color': image.select ? globalColors.secondary : null,
+				}"
 				@click="goToSlider(index, image)"
 			>
 				<picture>
-					<img 
-						:src="image.urlImage" 
+					<img
+						:src="image.urlImage"
 						@error="handleImageError"
-						:alt="image.name" 
+						:alt="image.name"
 						class="image-slider"
-					>
+					/>
 				</picture>
 			</button>
 		</div>
-		<div class="slider-product-view-web" v-if="webLocalImages && webLocalImages.length">
+		<div
+			class="slider-product-view-web"
+			v-if="webLocalImages && webLocalImages.length"
+		>
 			<swiper ref="mySwiper" :options="swiperOption">
-				<swiper-slide 
-					v-for="image in webLocalImages" 
-					:key="image.id">
-					<div
-						:class="['wrapper-image', { 'sold-out': noStock }]"
-						data-cy="presentation-img"
-					>
-						<img 
-							:src="image.urlImage" 
-							:alt="image.name"
-							@error="handleImageError"
-							class="image-product-slider"
-						>
+				<swiper-slide v-for="image in webLocalImages" :key="image.id" v-if="image.select">
+					<div :class="['wrapper-image', { 'sold-out': noStock }]" data-cy="presentation-img">
+					<img
+						:src="image.urlImage"
+						:alt="image.name"
+						@error="handleImageError"
+						class="image-product-slider"
+					/>
 					</div>
 				</swiper-slide>
 			</swiper>
 		</div>
-		<div
-			v-else
-			:class="['slider-product-view-one', { 'sold-out': noStock }]"
-		>
-			<img
-				:src="data.urlImage"
-				:alt="data.name"
-				class="image-product-slider"
-			>
+		<div v-else :class="['slider-product-view-one', { 'sold-out': noStock }]">
+			<img :src="data.urlImage" :alt="data.name" class="image-product-slider" />
 		</div>
-		<div class="slider-product-view-movil" v-if="movilLocalImages && movilLocalImages.length">
+		<div
+			class="slider-product-view-movil"
+			v-if="movilLocalImages && movilLocalImages.length"
+		>
 			<swiper ref="mySwiper" :options="swiperOption">
-				<swiper-slide 
-					v-for="image in movilLocalImages" 
-					:key="image.id">
+				<swiper-slide v-for="image in movilLocalImages" :key="image.id">
 					<div
 						:class="['wrapper-image', { 'sold-out': noStock }]"
 						data-cy="presentation-img"
 					>
-						<img 
-							:src="image.urlImage" 
+						<img
+							:src="image.urlImage"
 							:alt="image.name"
 							@error="handleImageError"
 							class="image-product-slider"
-						>
+						/>
 					</div>
 				</swiper-slide>
-				<div class="pagination-carousel swiper-pagination" slot="pagination"></div>
+				<div
+					class="pagination-carousel swiper-pagination"
+					slot="pagination"
+				></div>
 			</swiper>
 		</div>
 		<div
 			v-else
 			:class="['slider-product-view-one-movil', { 'sold-out': noStock }]"
 		>
-			<img
-				:src="data.urlImage"
-				:alt="data.name"
-				class="image-product-slider">
+			<img :src="data.urlImage" :alt="data.name" class="image-product-slider" />
 		</div>
 	</div>
 </template>
@@ -97,7 +91,7 @@ function swiper() {
 }
 
 function goToSlider(index, image) {
-	this.swiper.slideTo(index + 1, 1000, false);
+	// this.swiper.slideTo(index + 1, 1000, false);
 	this.webLocalImages = map(
 		setNewProperty('select', img => img.id === image.id),
 		this.webLocalImages,
@@ -130,20 +124,26 @@ function imagesHandler(newImages) {
 				this.movilLocalImages.push(img);
 			}
 		});
-
+		console.log(this.movilLocalImages);
+		console.log(this.webLocalImages);
 		if (this.movilLocalImages.length === 0) {
 			this.movilLocalImages = this.webLocalImages;
 		}
-		console.log(window.innerWidth);
-		console.log(this.webLocalImages.length === 0
-			&& this.movilLocalImages.length > 0 && window.innerWidth < 767);
-		if (this.webLocalImages.length === 0
-			&& this.movilLocalImages.length > 0 && window.innerWidth < 767) {
+		if (
+			this.webLocalImages.length === 0 &&
+			this.movilLocalImages.length > 0 &&
+			window.innerWidth < 767
+		) {
 			this.webLocalImages = this.movilLocalImages;
 		}
 		if (this.webLocalImages.length > 0) {
 			this.$set(this.webLocalImages[0], 'select', true);
 		}
+		console.log(this.movilLocalImages.length === 0);
+		console.log(this.webLocalImages.length === 0 &&
+			this.movilLocalImages.length > 0 &&
+			window.innerWidth < 767);
+		console.log(this.webLocalImages.length > 0);
 	}
 }
 
@@ -217,7 +217,9 @@ export default {
 			target.src = this.fallbackImage;
 		},
 		validProductImage(image) {
-			return image.urlImage && image.urlImage.trim() !== '' ? image.urlImage : this.fallbackImage;
+			return image.urlImage && image.urlImage.trim() !== ''
+				? image.urlImage
+				: this.fallbackImage;
 		},
 	},
 	props: {
@@ -243,164 +245,164 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-	.btn-product-view {
+.btn-product-view {
+	background: color(white);
+	border: 2px solid color(border);
+	border-radius: 7px;
+	height: 74px;
+	margin-bottom: 14px;
+	max-width: 94px;
+	transition: all 0.3s;
+
+	&.not-select {
+		opacity: 0.3;
+	}
+}
+
+.btns-product-view {
+	display: none;
+	flex-direction: column;
+	margin-right: 20px;
+	width: 94px;
+
+	@media screen and (min-width: 768px) {
+		display: flex;
+		height: fit-content;
+		position: sticky;
+		top: 135px;
+	}
+}
+
+.product-view {
+	display: flex;
+	justify-content: center;
+
+	@media screen and (max-width: 996px) {
+		border: 1px solid #f1eaea;
+		border-radius: 9px;
+		padding: 35px 9px 9px 9px;
+	}
+}
+
+.slider-product-view-web {
+	display: none;
+
+	@media screen and (min-width: 996px) {
 		background: color(white);
-		border: 2px solid color(border);
 		border-radius: 7px;
-		height: 74px;
-		margin-bottom: 14px;
-		max-width: 94px;
-		transition: all .3s;
-
-		&.not-select {
-			opacity: 0.3;
-		}
-	}
-
-	.btns-product-view {
-		display: none;
-		flex-direction: column;
-		margin-right: 20px;
-		width: 94px;
-
-		@media screen and (min-width: 768px) {
-			display: flex;
-			height: fit-content;
-			position: sticky;
-			top: 135px;
-		}
-	}
-
-	.product-view {
+		box-shadow: 0 2px 4px 0 rgba(213, 213, 213, 0.5);
 		display: flex;
-		justify-content: center;
-
-		@media screen and (max-width: 996px) {
-			border: 1px solid #f1eaea;
-			border-radius: 9px;
-			padding: 35px 9px 9px 9px;
-		}
+		height: fit-content;
+		padding: 0 19px;
+		position: sticky;
+		top: 135px;
+		width: 400px;
 	}
+}
 
-	.slider-product-view-web {
+.slider-product-view-movil {
+	background: transparent;
+	box-shadow: none;
+	display: flex;
+	height: 270px;
+	padding: 0;
+	width: 298px;
+
+	@media screen and (min-width: 996px) {
 		display: none;
-
-		@media screen and (min-width: 996px) {
-			background: color(white);
-			border-radius: 7px;
-			box-shadow: 0 2px 4px 0 rgba(213, 213, 213, 0.5);
-			display: flex;
-			height: fit-content;
-			padding: 0 19px;
-			position: sticky;
-			top: 135px;
-			width: 400px;
-		}
 	}
+}
 
-	.slider-product-view-movil {
-		background: transparent;
-		box-shadow: none;
-		display: flex;
-		height: 270px;
-		padding: 0;
-		width: 298px;
+.slider-product-view-one-movil {
+	width: 70%;
 
-		@media screen and (min-width: 996px) {
-			display: none;
-		}
+	@media screen and (min-width: 996px) {
+		display: none;
 	}
+}
 
-	.slider-product-view-one-movil {
-		width: 70%;
+.wrapper-image {
+	align-items: center;
+	display: flex;
+	height: 487px;
+	justify-content: center;
+	width: 100%;
 
-		@media screen and (min-width: 996px) {
-			display: none;
-		}
+	@media screen and (max-width: 996px) {
+		height: 217px;
 	}
+}
 
-	.wrapper-image {
+.image-product-slider {
+	width: 100%;
+
+	@media screen and (max-width: 996px) {
+		object-fit: contain;
+		max-height: 217px;
+	}
+}
+
+.slider-product-view-one-web {
+	display: none;
+
+	@media screen and (min-width: 996px) {
 		align-items: center;
+		background: color(white);
+		border-radius: 7px;
+		box-shadow: 0 2px 4px 0 rgba(213, 213, 213, 0.5);
 		display: flex;
 		height: 487px;
 		justify-content: center;
-		width: 100%;
-
-		@media screen and (max-width: 996px) {
-			height: 217px;
-		}
+		padding: 0 19px;
+		width: 80%;
 	}
+}
 
-	.image-product-slider {
-		width: 100%;
+.image-slider {
+	height: 70%;
+}
 
-		@media screen and (max-width: 996px) {
-			object-fit: contain;
-			max-height: 217px;
-		}
+.slider-product-view-one {
+	display: none;
+
+	@media screen and (min-width: 996px) {
+		display: block;
 	}
+}
 
-	.slider-product-view-one-web {
-		display: none;
+.sold-out {
+	overflow: hidden;
+	position: relative;
+
+	&::after {
+		align-items: center;
+		background-color: rgba(0, 0, 0, 0.6);
+		color: white;
+		content: 'Agotado';
+		display: flex;
+		font-size: 20px;
+		font-family: font(bold);
+		height: 60px;
+		justify-content: center;
+		position: absolute;
+		right: 34%;
+		top: 5%;
+		transform: rotate(-45deg);
+		width: 100%;
+		z-index: 99;
 
 		@media screen and (min-width: 996px) {
-			align-items: center;
-			background: color(white);
-			border-radius: 7px;
-			box-shadow: 0 2px 4px 0 rgba(213, 213, 213, 0.5);
-			display: flex;
-			height: 487px;
-			justify-content: center;
-			padding: 0 19px;
-			width: 80%;
+			font-size: 30px;
 		}
 	}
+}
 
-	.image-slider {
-		height: 70%;
-	}
-
-	.slider-product-view-one {
-		display: none;
-
-		@media screen and (min-width: 996px) {
-			display: block;
-		}
-	}
-
-	.sold-out {
-		overflow: hidden;
-		position: relative;		
-
-		&::after {
-			align-items: center;
-			background-color: rgba(0,0,0,0.6);
-			color: white;
-			content: 'Agotado';
-			display: flex;
-			font-size: 20px;
-			font-family: font(bold);
-			height: 60px;
-			justify-content: center;
-			position: absolute;
-			right: 34%;
-			top: 5%;
-			transform: rotate(-45deg);
-			width: 100%;
-			z-index: 99;
-
-			@media screen and (min-width: 996px) {
-				font-size: 30px;
-			}
-		}
-	}
-
-	.btn-unit {
-		border: 1px solid #dedede;
-		border-radius: 16px;
-		color: #dedede;
-		font-size: 10px;
-		height: 25px;
-		width: 53px;
-	}
+.btn-unit {
+	border: 1px solid #dedede;
+	border-radius: 16px;
+	color: #dedede;
+	font-size: 10px;
+	height: 25px;
+	width: 53px;
+}
 </style>
